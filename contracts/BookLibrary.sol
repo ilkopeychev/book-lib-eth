ea0b081a3ed74bc3098bf55df3f4d433dd8ca48d
ilkopeychev <ilkokpeychev@gmail.com>
2021-10-18 18:06:56 +0300

    updated with struct and nested maps

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -5,76 +5,48 @@ import "./Ownable.sol";
 
 contract BookLib is Ownable {     //  REMINDER DO NOT USE ARRAYs, care with mapping sync, try to use memory instead of storrage whereven you can, watch transaction expence
 
-    // address
     
- 
-
-    address[] private peopleAddresses;      // address of all people that took books
-    
-    mapping(address=>BorrowedBook) private addressToOwner; // addresses of the owners of the books
-    mapping(bytes32=>Book) private bytesToBooks;
+    mapping(address=>mapping(bytes32=>bool)) private addressToOwner; // addresses of the owners of the books
+    mapping(bytes32=>Book) public bytesToBooks;
     
     bytes32[] private availableBooksIds;
     
     struct Book {
         string name;
         uint count;
-        // mapping(address=>mapping(bytes32=>Book)) bookOwners;
+        address[] borowers;
     }
-    
-    struct BorrowedBook {
-        bytes32 id;
-        bool isBorrowed;
-        bool isValue; 
-    }
-    
     
     // only administrator can add books
     function addBook(string calldata _name, uint _count) public onlyOwner {
-        bytes32 bookByteId = stringToBytes32(_name); 
-        bytesToBooks[bookByteId]=Book(_name,_count);     // BOOKS name to bytes and use it for ID
+        address[] memory borrowed;
+        bytes32 bookByteId = keccak256(abi.encodePacked(_name));
+        bytesToBooks[bookByteId]=Book(_name,_count,borrowed);     // BOOKS name to bytes and use it for ID
         availableBooksIds.push(bookByteId);
     }
 
     // user can borrow just 1 copy of a book
     function borrowBook(bytes32 _id) public  {
-        require(bytesToBooks[_id].count>0 && addressToOwner[msg.sender].isBorrowed==false);
-        if(!addressToOwner[msg.sender].isValue) {
-             peopleAddresses.push(msg.sender);
-        }
-        
+        require(bytesToBooks[_id].count>0 && addressToOwner[msg.sender][_id]==false);
+        bytesToBooks[_id].borowers.push(msg.sender);
         bytesToBooks[_id].count--;
-        addressToOwner[msg.sender]=BorrowedBook(_id,true,true);
+        addressToOwner[msg.sender][_id]=true;
     }
     
     // return borrowBook
     function returnBorrowedBook(bytes32 _id) public {
-        addressToOwner[msg.sender].isBorrowed=false;
+        addressToOwner[msg.sender][_id]=false;
         bytesToBooks[_id].count++;
     }
     
-    // show all owners that BorrowedBooks 
-     function getOwnersAdrressesBooks() public view returns (address[] memory){
-        return peopleAddresses;
+    // show all owners that Borrowed that Book
+     function getBorowersBookAddreses(bytes32 _id) public view returns (address[] memory){
+        return bytesToBooks[_id].borowers;
     }
     
     // return available books
     function getAvailableBookIds() public view returns (bytes32[] memory bookIDS) {
         bookIDS = availableBooksIds;
     }
-    
-    // function getBook(bytes32 _id) public view returns(Book memory) {
-    //     return bytesToBooks[_id];
-    // }
 
-    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
-        if (tempEmptyStringTest.length == 0) {
-            return 0x0;
-        }
-
-        assembly {
-            result := mload(add(source, 32))
-        }
-    }
 }
